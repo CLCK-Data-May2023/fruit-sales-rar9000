18e6d7875ef3afeb20689ed0367d98fbd593de61
rar9000 <clavius@Clavius.local>
2023-09-02 15:23:15 -0400

    assignment completed

--- fruitsales.py
+++ fruitsales.py
@@ -1,1 +1,5 @@
-# add your code here
+import pandas as pd
+fruit_sales = pd.DataFrame([[35, 21], [41, 34]], columns=['Apples', 'Bananas'],
+                           index=['2017 Sales', '2018 Sales'])
+df = pd.DataFrame(fruit_sales)
+df.to_csv('fruit.csv')
